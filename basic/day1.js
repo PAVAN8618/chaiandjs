2e69805af5e318b3f85a9753a1937da3bf8885ee
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-24 06:17:59 +0000

    ds

--- basic/day1.js
+++ basic/day1.js
@@ -1,1 +1,22 @@
-console.log("Pawan");
+//console.log("Pawan");
+
+const users={
+    name:"pawan",
+    gmail:"pnttiwari99@gmail.com",
+    mobileNumber:8618424406
+}
+
+const users2={
+    1:"pawan2",
+    2:"pnttiwari99@gmail.com2",
+    "ram":86184244062
+}
+const users3 ={...users, ...users2}
+//console.log(users3)
+//console.log(Object.keys(users3))
+//console.log(Object.entries(users))
+
+const {ram} =users2
+console.log(ram);
+
+
