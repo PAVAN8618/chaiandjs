44e22541ebf70c4e7b27d8011c88967fa1fcea4c
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-25 14:13:06 +0000

    day4

--- basic/day4.js
+++ basic/day4.js
@@ -13,26 +13,25 @@ let str = "String primitives and string ";
 //     console.log(str[iterator]);
 // }
 
-//console.log(str.toLocaleUpperCase());//STRING PRIMITIVES AND STRING 
+console.log(str.toLocaleUpperCase());//STRING PRIMITIVES AND STRING 
 
-// console.log(str.toLocaleLowerCase())
+ console.log(str.toLocaleLowerCase())
 /*STRING PRIMITIVES AND STRING 
 string primitives and string */
 
 
-//console.log(str.split(" ")); //[ 'String', 'primitives', 'and', 'string', '' ]
+console.log(str.split(" ")); //[ 'String', 'primitives', 'and', 'string', '' ]
 
-//console.log(str.concat("pawan"));
+console.log(str.concat("pawan"));
 
-//console.log(str.startsWith("s"))//fasle
-//console.log(str.valueOf(1));
+console.log(str.startsWith("s"))//fasle
+console.log(str.valueOf(1));
 
-//console.log(str.trim())// white space remove
+console.log(str.trim())// white space remove
 
 //console.log(str.includes("an]d"));//false
-
-// //console.log(str.indexOf("and"))//
-// console.log(str.replace("string","hero"));//String primitives and hero 
+console.log(str.indexOf("and"))//
+ console.log(str.replace("string","hero"));//String primitives and hero 
 
 console.log(str.slice(3,9));//ing pr
 
