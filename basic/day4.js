247c7f764dbe71b5e60edf4f38fbce4d85b64fbf
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-25 14:10:40 +0000

    day4

--- basic/day4.js
+++ basic/day4.js
@@ -1,1 +1,43 @@
-console.log("day4")
+// console.log("day4")
+
+// new function (a,b) {
+//     return a+b;
+//   }(2,3);
+
+
+let obj = Object();
+// console.log(obj)
+
+let str = "String primitives and string ";
+// for (const iterator in str) {
+//     console.log(str[iterator]);
+// }
+
+//console.log(str.toLocaleUpperCase());//STRING PRIMITIVES AND STRING 
+
+// console.log(str.toLocaleLowerCase())
+/*STRING PRIMITIVES AND STRING 
+string primitives and string */
+
+
+//console.log(str.split(" ")); //[ 'String', 'primitives', 'and', 'string', '' ]
+
+//console.log(str.concat("pawan"));
+
+//console.log(str.startsWith("s"))//fasle
+//console.log(str.valueOf(1));
+
+//console.log(str.trim())// white space remove
+
+//console.log(str.includes("an]d"));//false
+
+// //console.log(str.indexOf("and"))//
+// console.log(str.replace("string","hero"));//String primitives and hero 
+
+console.log(str.slice(3,9));//ing pr
+
+//slice return staring(first paremeter ) point and last(second parementer) to one before value
+
+
+
+
