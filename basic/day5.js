b68a1e9bdf45a8c0a12431799f43b0b85e67d342
Pavan Kumar Tiwari <55940294+PAVAN8618@users.noreply.github.com>
2023-08-25 14:31:06 +0000

    day5

---
+++ basic/day5.js
@@ -0,0 +1,19 @@
+//Array basic
+
+let arr = Array("pawan","2",3,4,5,6,7,8,9,10)
+//console.log(arr);
+
+arr.forEach(element => {
+    //console.log(element +arr[1]);
+});
+//console.log(arr.concat(...arr,5))
+let ar2 =arr.concat(11111,1,5)
+// console.log(ar2);
+
+// console.log(arr.push(4))//
+// console.log(arr.pop(4))//
+let pop = arr.pop()
+// console.log(pop)
+arr.push(99)
+
+console.log(arr)
